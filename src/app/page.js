31952699d4227997a85cccc9446ea3aa5df5a777
Gaurav kumar <68615047+i-m-Gaurav@users.noreply.github.com>
2023-08-14 12:39:54 +0530

    Update page.js fixed the card 2idth issue

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import TodoLists from './components/TodoLists'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
      <TodoLists/>
     
     </main>
